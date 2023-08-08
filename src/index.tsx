c20e4143204f30c797dfb516107adfddd01b1d5b
Methma Peiris <61134846+MethmaPeiris99@users.noreply.github.com>
2023-08-08 15:29:59 +0530

    Route home and mentor profile components

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home';
 import MainLayout from './components/Layout/MainLayout';
+import MentorProfile from './components/MentorProfile/MentorProfile';
 
 const App: React.FC = () => (
-  <MainLayout>
-    <Home />
-  </MainLayout>
+  <BrowserRouter>
+    <MainLayout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mentor/:mentorId" element={<MentorProfile />} />
+      </Routes>
+    </MainLayout>
+  </BrowserRouter>
 );
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = createRoot(document.getElementById('root'));
+rootElement.render(<App />);
